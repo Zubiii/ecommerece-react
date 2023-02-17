9370fc07f60215d1edd3d383057de2e3b0e71804
zubiii <zubair.javed.ch@gmail.com>
2023-02-17 18:40:04 +0500

    add styling and make component for the category item

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import "./categories.styles.scss"
+import CategoryItem from './components/category/index'
+
 const  App = () => {
   const categories = [
     {
@@ -31,16 +32,8 @@ const  App = () => {
   return (
     <div className="categories-container">
       {
-        categories.map(({title, id, imageUrl}) => (
-          <div id={id} className="category-container">
-            <div className="background-image" style={{
-              backgroundImage: `url(${imageUrl})`
-            }}/>
-            <div className="category-body-container">
-              <h1>{title.toUpperCase()}</h1>
-              <p>Shop Now</p>
-            </div>
-          </div>
+        categories.map(category => (
+          <CategoryItem key={category.id} title={category.title} imageUrl={category.imageUrl} />
         ))
       }
     </div>
---
+++ src/components/category/index.js
@@ -0,0 +1,17 @@
+import './categories.styles.scss'
+
+const CategoryItem = ({title, imageUrl}) => {
+    return (
+        <div className="category-container">
+            <div className="background-image" style={{
+              backgroundImage: `url(${imageUrl})`
+            }}/>
+            <div className="category-body-container">
+              <h1>{title}</h1>
+              <p>Shop Now</p>
+            </div>
+        </div>
+    )
+}
+
+export default CategoryItem
