c3b64a50d5414c1fa4274022815d83bb2a80efc4
zubiii <zubair.javed.ch@gmail.com>
2023-02-20 04:51:43 +0500

    add nevigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import { Routes, Route} from 'react-router-dom'
+import Nevigation from './components/NevigationBar/index'
 import Home from './pages/home/index'
 
 const  App = () => {
+
+  const Shop = () => {
+    return (
+      <h1>I'm in the Shop!</h1>
+    )
+  }
   
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Nevigation/>}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom'
 import CategoryItem from '../../components/category/index'
 
 const  Home = () => {
